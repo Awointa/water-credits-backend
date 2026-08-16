59d3c956f639a5e0f5919095aa4b70551d9cff08
Awointa <akosawointa@gmail.com>
2026-08-16 13:43:05 +0100

    fix: merge duplicate @Module blocks in oracle.module.ts

--- src/modules/oracle/oracle.module.ts
+++ src/modules/oracle/oracle.module.ts
@@ -6,18 +6,14 @@ import { OracleService } from './oracle.service';
 import { OracleProcessor } from './oracle-processor';
 import { OracleSubmission } from './entities/oracle-submission.entity';
 import { GovernanceConfig } from '../governance/entities/governance-config.entity';
+import { SensorReading } from '../sensors/entities/sensor-reading.entity';
 
 @Module({
   imports: [
     // GovernanceConfig is needed by OracleProcessor to snapshot config at
     // batch-start (Issue #34).  GovernanceModule also registers it; TypeORM
     // deduplicates the underlying repository provider.
-    TypeOrmModule.forFeature([OracleSubmission, GovernanceConfig]),
-import { SensorReading } from '../sensors/entities/sensor-reading.entity';
-
-@Module({
-  imports: [
-    TypeOrmModule.forFeature([OracleSubmission, SensorReading]),
+    TypeOrmModule.forFeature([OracleSubmission, GovernanceConfig, SensorReading]),
     BullModule.registerQueue({
       name: 'oracle-submit',
       defaultJobOptions: {
